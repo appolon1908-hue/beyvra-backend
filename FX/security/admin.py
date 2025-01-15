db0ee3de23a37b9f64652fcdcf0e241b2370dbf8
henry-akwerigbe <Henry.Akwerigbe@thras.io>
2025-01-15 20:58:29 +0100

    added country_blacklist, ip_blacklist, user_blacklist to admin panel

--- FX/security/admin.py
+++ FX/security/admin.py
@@ -5,34 +5,71 @@ from security import models
 
 
 class UserActivityAdmin(admin.ModelAdmin):
-    list_display = ('user', 'action_type', 'action_status', 'created_at')
-    list_filter = ('action_type', 'action_status')
-    search_fields = ('user__email', 'ip_address', 'user_agent')
+    list_display = (
+        "user",
+        "anonymous_user",
+        "action_type",
+        "description",
+        "action_status",
+        "created_at",
+    )
+    list_filter = ("action_type", "action_status")
+    search_fields = ("user__email", "ip_address", "user_agent")
 
 
 admin.site.register(models.UserActivity, UserActivityAdmin)
 
 
 class IPWhitelistAdmin(admin.ModelAdmin):
-    list_display = ('ip_address', 'created_at')
-    search_fields = ('ip_address',)
+    list_display = ("ip_address", "created_at")
+    search_fields = ("ip_address",)
 
 
 admin.site.register(models.IPWhitelist, IPWhitelistAdmin)
 
 
+class IPBlacklistAdmin(admin.ModelAdmin):
+    list_display = ("ip_address", "created_at")
+    search_fields = ("ip_address",)
+
+
+admin.site.register(models.IPBlacklist, IPBlacklistAdmin)
+
+
 class CountryWhitelistAdmin(admin.ModelAdmin):
-    list_display = ('country', 'created_at')
-    search_fields = ('country',)
+    list_display = ("country", "created_at")
+    search_fields = ("country",)
 
 
 admin.site.register(models.CountryWhitelist, CountryWhitelistAdmin)
 
 
+class CountryBlacklistAdmin(admin.ModelAdmin):
+    list_display = ("country", "created_at")
+    search_fields = ("country",)
+
+
+admin.site.register(models.CountryBlacklist, CountryBlacklistAdmin)
+
+
+class UserBlacklistAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+    search_fields = ("user",)
+
+
+admin.site.register(models.UserIPBlacklist, UserBlacklistAdmin)
+
+
 class IPRestrictionsAdmin(admin.ModelAdmin):
-    list_display = ('admin', 'restriction_type')
-    search_fields = ('admin__email', 'restriction_type')
-    filter_horizontal = ('ip_whitelist', 'country_whitelist', 'ip_blacklist')
+    list_display = ("admin", "restriction_type")
+    search_fields = ("admin__email", "restriction_type")
+    filter_horizontal = (
+        "ip_whitelist",
+        "country_whitelist",
+        "country_blacklist",
+        "ip_blacklist",
+        "user_ip_blacklist",
+    )
 
 
 admin.site.register(models.IPRestrictions, IPRestrictionsAdmin)
