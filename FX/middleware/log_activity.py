c07a133fa8c4f425a20727a0facc0bc5c7f5c820
henry-akwerigbe <Henry.Akwerigbe@thras.io>
2025-01-18 21:39:19 +0100

    - enabled log function to capture activity without sending a request

--- FX/middleware/log_activity.py
+++ FX/middleware/log_activity.py
@@ -2,6 +2,7 @@ import logging
 
 from django.db import transaction
 from security.models import UserActivity, UserActivityActionStatus, UserActivityActionTypes
+from users.models import UserDeviceInfo
 from users.utils import get_ip_address, get_user_agent, get_user_location_mod
 
 logger = logging.getLogger(__name__)
@@ -15,14 +16,15 @@ def log(
     status_code=200,
     accepted_status_code: tuple[int, any] = (200, 201, 302, 301),
     user=None,
+    user_device_info: UserDeviceInfo = None,
 ):
     try:
         # Gather request metadata
-        user_agent = get_user_agent(request)
-        ip_address = get_ip_address(request)
-        user_location = get_user_location_mod(ip_address)
-        device_type = user_agent["device_type"]
-        device_model = user_agent["device_model"]
+        user_agent = get_user_agent(request) if request else user_device_info.user_agent
+        ip_address = get_ip_address(request) if request else user_device_info.ip_address
+        user_location = get_user_location_mod(ip_address) if request else user_device_info.location
+        device_type = user_agent["device_type"] if request else user_device_info.device
+        device_model = user_agent["device_model"] if request else user_device_info.device
 
         # Using transaction to ensure data integrity
         data = {
@@ -39,7 +41,7 @@ def log(
             "geolocation": user_location,
             "device_type": device_type,
             "device_model": device_model,
-            "user_agent": user_agent["user_agent"],
+            "user_agent": user_agent["user_agent"] if request else user_device_info.user_agent,
         }
         with transaction.atomic():
             UserActivity.objects.create(**data)
