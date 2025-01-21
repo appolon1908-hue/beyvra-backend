da1228523c092e9ac8680ce3a6fed113e1ad4855
henry-akwerigbe <Henry.Akwerigbe@thras.io>
2025-01-21 12:42:13 +0100

    - added country_blacklist, ip_blacklist, user_blacklist to admin panel

--- FX/security/admin.py
+++ FX/security/admin.py
@@ -21,6 +21,14 @@ class IPWhitelistAdmin(admin.ModelAdmin):
 admin.site.register(models.IPWhitelist, IPWhitelistAdmin)
 
 
+class IPBlacklistAdmin(admin.ModelAdmin):
+    list_display = ("ip_address", "created_at")
+    search_fields = ("ip_address",)
+
+
+admin.site.register(models.IPBlacklist, IPBlacklistAdmin)
+
+
 class CountryWhitelistAdmin(admin.ModelAdmin):
     list_display = ("country", "created_at")
     search_fields = ("country",)
@@ -29,10 +37,32 @@ class CountryWhitelistAdmin(admin.ModelAdmin):
 admin.site.register(models.CountryWhitelist, CountryWhitelistAdmin)
 
 
+class CountryBlacklistAdmin(admin.ModelAdmin):
+    list_display = ("country", "created_at")
+    search_fields = ("country",)
+
+
+admin.site.register(models.CountryBlacklist, CountryBlacklistAdmin)
+
+
+class UserBlacklistAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+    search_fields = ("user",)
+
+
+admin.site.register(models.UserIPBlacklist, UserBlacklistAdmin)
+
+
 class IPRestrictionsAdmin(admin.ModelAdmin):
     list_display = ("admin", "restriction_type")
     search_fields = ("admin__email", "restriction_type")
-    filter_horizontal = ("ip_whitelist", "country_whitelist", "ip_blacklist")
+    filter_horizontal = (
+        "ip_whitelist",
+        "country_whitelist",
+        "country_blacklist",
+        "ip_blacklist",
+        "user_ip_blacklist",
+    )
 
 
 admin.site.register(models.IPRestrictions, IPRestrictionsAdmin)
