0a51b5cff6c7062d0e61d02ed326f19ad985e485
Emeka <emeka@codestra.digital>
2025-01-22 19:55:07 -0800

    Added log connections and disconnection events

--- FX/wsnotifications/consumers/base.py
+++ FX/wsnotifications/consumers/base.py
@@ -3,7 +3,12 @@ from channels.db import database_sync_to_async
 from django.contrib.auth.models import User
 
 from django.core.cache import cache
-from wsnotifications.utils import db_online_users_count, db_user_connected, db_user_disconnected, can_access_group
+from wsnotifications.utils import db_online_users_count, db_user_connected, db_user_disconnected
+
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class BaseConsumer(AsyncJsonWebsocketConsumer):
@@ -15,18 +20,17 @@ class BaseConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept()
         user = self.scope['user']
-        updated_user = await db_user_connected(user)
-        print(updated_user)
-        result = await db_online_users_count()
-        ##result of active users will be sent to admin
-        print(result)
+        logger.info(f"Base Consumer {user}")
+        await db_user_connected(user)
+        await db_online_users_count()
+        
         
         
     async def disconnect(self, close_code):
         user = self.scope['user']
         print(user)
         await db_user_disconnected(user)
-        result =  await db_online_users_count(user)
+        result =  await db_online_users_count()
         print(result)
         await self.close()
 
--- FX/wsnotifications/utils.py
+++ FX/wsnotifications/utils.py
@@ -3,44 +3,45 @@ from users.models import User
 from django.db.models import Count, Q
 from django.core.cache import cache
 import uuid
+import logging
 
+logger = logging.getLogger(__name__)
 
-#1:f06e75bc-55ef-4eeb-a559-4d0fa0df2ace
-#2:6ae71e8b-41f3-4a89-bc05-02c0ea57d3ed
-
-
-
-@database_sync_to_async
-def can_access_group(self):
-    """Check if user can access the requested group"""
-    if self.user.is_staff:
-        return self.group_name in self.GROUPS['admin']
-    return self.group_name in self.GROUPS['user']
 
 @database_sync_to_async
 def db_user_connected(user: User) -> User:
     if user.is_authenticated:
+        logger.info(user)
+        logger.info(f"User {user.email} connected.")
         User.objects.filter(id=user.id).update(is_online=True)
-        user.is_online = True  # Update instance to maintain consistency
+        user.is_online = True
         return user
-
+    
+    
+def get_id(id: str):
+    return id
+    
+    
 
 @database_sync_to_async
 def db_online_users_count():
     ##Added cache to optimize database
     cache_key = 'online_users_count'
     cached_result = cache.get(cache_key)
-    print(cached_result)
+    logger.info(cached_result)
     if cached_result is None:
         counts = User.objects.aggregate(
         regular_users=Count('id', filter=Q(is_online=True, is_staff=False)),
         admin_users=Count('id', filter=Q(is_online=True, is_staff=True))
         )
-    return {
-        'regular_users': counts['regular_users'],
-        'admin_users': counts['admin_users'],
-        'total_users': counts['regular_users']+ counts['admin_users']
-    }
+        data = {
+            'regular_users': counts['regular_users'],
+            'admin_users': counts['admin_users'],
+            'total_users': counts['regular_users']+ counts['admin_users']
+        }
+        cache.set('online_users_count', data, timeout=None)
+        return data
+    return cached_result
 
 
 @database_sync_to_async
