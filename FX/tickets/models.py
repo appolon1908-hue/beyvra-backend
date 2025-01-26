157eb356f1b88cc48a445fac741f7bf5dddfc8b9
henry-akwerigbe <Henry.Akwerigbe@thras.io>
2025-01-26 16:16:20 +0100

    - added ticket status field to ticket model

--- FX/tickets/models.py
+++ FX/tickets/models.py
@@ -1,7 +1,17 @@
 import uuid
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from users.models import User
+
+
+class TicketStatus(models.TextChoices):
+    PENDING = "Pending", _("Pending")
+    IN_PROGRESS = "In_Progress", _("In Progress")
+    ESCALATED = "Escalated", _("Escalated")
+    RESOLVED = "Resolved", _("Resolved")
+    CANCELLED = "Cancelled", _("Cancelled")
+    REOPENED = "Reopened", _("Reopened")
 
 
 class SupportTicket(models.Model):
@@ -9,6 +19,7 @@ class SupportTicket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=255, null=True, blank=True)
     message = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=TicketStatus.choices, default=TicketStatus.PENDING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     assigned_admin = models.ForeignKey(
