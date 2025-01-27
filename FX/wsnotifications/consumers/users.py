94ef58c040ddbf6be090ef4337b77abc8d95f822
Emeka <emeka@codestra.digital>
2025-01-27 16:58:31 +0100

    Added PriceAlertConsumers in UserConsumer

--- FX/wsnotifications/consumers/users.py
+++ FX/wsnotifications/consumers/users.py
@@ -36,6 +36,34 @@ class UserConsumer(BaseConsumer):
 
     async def send_message(self, event):
         await user_handlers.dispatch_message(self, event)
+        
+        
+        
+class PriceThresholdConsumer(BaseConsumer):
+    """
+    Handles real-time trade data updates including prices,
+    volumes, and other trade indicators.
+    """
+    
+    async def connect(self):
+        await super().connect()
+        user = self.scope['user']
+        if user.is_authenticated:
+            group_name = f"pricethreshold_{user.id}"
+            logger.info("Price Threshold Added")
+            await self.channel_layer.group_add(group_name, self.channel_name)   
+        else:
+            await super().disconnect()
+
+
+    async def disconnect(self, close_code):
+        user =self.scope['user']
+        await self.channel_layer.group_discard(f"user_{user.id}", self.channel_name)
+        await self.channel_layer.group_add(f"users", self.channel_name)
+        await super().disconnect(close_code)
+
+    async def send_message(self, event):
+        await user_handlers.dispatch_message(self, event)
 
 
 
