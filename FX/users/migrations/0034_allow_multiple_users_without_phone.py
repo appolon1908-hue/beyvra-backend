ffbad4a10586d5505439e8ebe73a86faf598c193
root <root@Ubuntu-jammy-latest-amd64-base.zst>
2026-08-13 02:43:24 +0000

    fix(migrations): normalize optional phones after schema change

--- FX/users/migrations/0034_allow_multiple_users_without_phone.py
+++ FX/users/migrations/0034_allow_multiple_users_without_phone.py
@@ -3,16 +3,28 @@ from django.db import migrations, models
 import users.utils
 
 
+def normalize_missing_phones(apps, schema_editor):
+    user = apps.get_model("users", "User")
+    user.objects.filter(phone_number="").update(phone_number=None)
+
+
+def restore_legacy_missing_phone(apps, schema_editor):
+    user = apps.get_model("users", "User")
+    missing = user.objects.filter(phone_number__isnull=True)
+    if missing.count() > 1:
+        raise RuntimeError(
+            "Cannot restore the legacy non-null phone constraint after multiple "
+            "phone-less identities have been created."
+        )
+    missing.update(phone_number="")
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("users", "0033_remove_stale_verification_fields"),
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql="UPDATE users_user SET phone_number = NULL WHERE phone_number = ''",
-            reverse_sql=migrations.RunSQL.noop,
-        ),
         migrations.AlterField(
             model_name="user",
             name="phone_number",
@@ -24,4 +36,5 @@ class Migration(migrations.Migration):
                 validators=[users.utils.PHONE_REGEX_VALIDATOR],
             ),
         ),
+        migrations.RunPython(normalize_missing_phones, restore_legacy_missing_phone),
     ]
